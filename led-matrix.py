66a5e175bf08f77346429966aa36fdf9f10691e0
Felix Na <felix.na70@gmail.com>
2022-11-27 11:38:36 +0100

    add comment

--- led-matrix.py
+++ led-matrix.py
@@ -1,3 +1,11 @@
+#
+# led-matrix.py
+#
+# grid with diffusor for 16x16 LED-matrix (https://de.aliexpress.com/item/4000544584524.html)
+#
+# Print with 0.2 mm layer height, first two layers in white filament and then filament change to black for the grid matrix
+#
+
 import cadquery as cq
 
 pcb_x = 160.0 # PCB x size
